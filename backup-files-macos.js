a2d0003ea3767221cf034a118b16b4875ee45864
Felipe Melgaço <fmagesty@gmail.com>
2023-03-01 11:18:03 -0300

    update backup script to accept CLI args/env vars
    also added a little 'how-to-run' comment just to keep it simple but more descriptive

--- backup-files-macos.js
+++ backup-files-macos.js
@@ -3,9 +3,12 @@ const path = require('path');
 const { exec } = require('child_process');
 
 // Define the source and destination folders for the backup
-const sourceFolder = '/Users/your-username/Desktop/my-folder';
-const backupFolder = '/Users/your-username/Backups/my-folder';
-
+// const sourceFolder = process.env.SOURCE_FOLDER || '/Users/your-username/Desktop/my-folder';
+// const backupFolder = process.env.BACKUP_FOLDER || '/Users/your-username/Backups/my-folder';
+const sourceFolder = process.argv[2] || '/Users/your-username/Desktop/my-folder';
+const backupFolder = process.argv[3] || '/Users/your-username/Backups/my-folder';
+// const sourceFolder = '/Users/your-username/Desktop/my-folder';
+// const backupFolder = '/Users/your-username/Backups/my-folder';
 // Create the backup folder if it doesn't already exist
 if (!fs.existsSync(backupFolder)) {
   fs.mkdirSync(backupFolder, { recursive: true });
@@ -32,3 +35,8 @@ fs.readdir(sourceFolder, (err, files) => {
   // Open the backup folder in Finder
   exec(`open ${backupFolder}`);
 });
+
+
+// running through cli example with node:
+// node backup-files-macos.js '/Users/username/someFolder/sourceFolder' '/Users/username/someFolder/backupFolder'
+// and this will copy the source content to the backup path
